80bb057397f434d4cb92d444590e0978907fa916
Zabi1112 <66674164+Zabi1112@users.noreply.github.com>
2024-09-23 15:18:47 +0500

    Types Changed

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,10 +6,9 @@ function Type() {
     <Typewriter
       options={{
         strings: [
+          "Game Developer",
           "Software Developer",
           "Freelancer",
-          "Game Developer",
-          "Open Source Contributor",
         ],
         autoStart: true,
         loop: true,
