a77e99c3c99b3a3af09aa86e7a06012f45b2b196
Zabi1112 <66674164+Zabi1112@users.noreply.github.com>
2024-09-23 11:11:54 +0500

    Changes For Project Cards and Details With Tech Stacks

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,10 +27,21 @@ function Projects() {
               isBlog={false}
               videoLink="https://www.youtube.com/embed/7DBj2WDapJE"
               title="Gnome Wars"
-              description="It’s a Webgl Game. It is a multiplayer shooting game with a unique concept in which player
+              description="
+"               
+description=
+{<>
+  It’s a Webgl Game. It is a multiplayer shooting game with a unique concept in which player
                and ai characters are gnomes. As a game developer i worked in this game fetching data from API,
-                Multiplayer functionality, Game Play Look and Feel, Game Modes, Google SSO, Twitter SSO
-"
+                Multiplayer functionality, Game Play Look and Feel, Game Modes, Google SSO, Twitter SSO.<br />
+<strong>Tech Stack:</strong><br />
+- Unity
+- C#
+- WebGL
+- Node
+- Mirror
+- JavaScript
+</>}
               // ghLink="https://github.com/soumyajit4419/Chatify"
               // demoLink="https://chatify-49.web.app/"
             />
@@ -43,11 +54,20 @@ function Projects() {
               videoLink="https://www.youtube.com/embed/G5k-pBDwP9k"
 
               title="Phonics Zoom"
-              description="Its a 2d Learning App with multiple mini games in it.
+              description=
+                {<>
+                  Its a 2d Learning App with multiple mini games in it.
                It is a client game for his school in which we made different games to teach students spelling paragraph
                 writing and many more games like this as  game developer i have created many games in this i also fetched
                 apis from the web portal where teachers can update the spelling
-                lists and also i posts the result of the students back to portal."
+                lists and also i posts the result of the students back to portal.<br />
+      <strong>Tech Stack:</strong><br />
+          - Unity
+          - C#
+          - Android
+          - WebGL
+          - Html
+          </>}
               // ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
               // demoLink="https://blogs.soumya-jit.tech/"
             />
@@ -59,11 +79,19 @@ function Projects() {
               isBlog={false}
               videoLink="https://www.youtube.com/embed/xqlKlvH26gE"
               title="Moon Karts"
-              description="It’s a WebGL Game for a client which will be uploaded on a metaverse named as
+              description={<>
+              It’s a WebGL Game for a client which will be uploaded on a metaverse named as
               splinter lad for Arcade Colony. As a unity developer I worked on its game play development
               and multiplayer work and its smoothness. I also integrate the sso system by building js
-               libraries in unity. Also the app is deployed for mobile.
-"
+               libraries in unity. Also the app is deployed for mobile.<br />
+  <strong>Tech Stack:</strong><br />
+      - Unity
+      - C#
+      - Android
+      - WebGL
+      - Photon Quantum
+      - Node
+      </>}
               // ghLink="https://github.com/soumyajit4419/Editor.io"
               // demoLink="https://editor.soumya-jit.tech/"
             />
@@ -71,19 +99,29 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              videoLink="https://www.youtube.com/embed/pIH7rIzS_U4"
-
-              title="AI Layer Moba"
-              description="It’s a WebGL Game. It's  just a game in progress right now.
+             imgPath={suicide}
+             isBlog={false}
+             videoLink="https://www.youtube.com/embed/pIH7rIzS_U4"
+             title="Ai Layer Moba"
+             description=
+             {<>
+               It’s a WebGL Game. It's  just a game in progress right now.
                It will be a game like dota in which we have characters with their special abilities
                and in game level progression and within game level progress characters
                 also transform shape.In this we have blockchain wires sso wallet
-                integration and many other features.
-"
-              // ghLink="https://github.com/soumyajit4419/Plant_AI"
-              // demoLink="https://plant49-ai.herokuapp.com/"
+                integration and many other features.<br />
+               <strong>Tech Stack:</strong><br />
+                   - Unity
+                   - C#
+                   - WebGL
+                   - JavaScript
+                   - Node
+                   </>}
+             
+            // ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+             // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+           />
+              
             />
           </Col>
 
@@ -93,7 +131,16 @@ function Projects() {
               isBlog={false}
               videoLink="https://www.youtube.com/embed/HiZBZ_ydkoQ"
               title="Horse Stable Game"
-              description="It is a single player game of horse racing in which you have to take care of horse maintain his health and make him ready for the races"
+              description=
+              {<>
+               It is a single player game of horse racing in which you have to take care of horse maintain his health and make him ready for the races.<br />
+                <strong>Tech Stack:</strong><br />
+                    - Unity
+                    - C#
+                    - Android
+                    - IOS
+                    </>}
+              
              // ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
@@ -106,7 +153,16 @@ function Projects() {
 
               videoLink="https://www.youtube.com/embed/6clb09voJD0"
               title="Vampire Fall"
-              description="It is a single Player game in vampire attacks the player and player has to defend himself in it "
+              description={<>
+                It is a multiplayer Player RPG game in vampire attacks the player and player has to defend himself and town and also build the town and it safety measures. <br />
+                <strong>Tech Stack:</strong><br />
+                    - Unity
+                    - Node
+                    - C#
+                    - Android
+                    - IOS
+                    - Unet4
+                    </>}
               // ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
             />
@@ -118,8 +174,16 @@ function Projects() {
 
               videoLink="https://www.youtube.com/embed/GAmkGG-Lot4"
               title="Family farm Sea Side 2D"
-              description="It is a 2d Builder game in which user manages a farm have in apps sell products and make his farm some top level.
-               leader board and many more apis"
+              description={<>
+              It is a 2d Builder game in which user manages a farm have in apps sell products and make his farm some top level.
+              leader board and many more apis<br />
+              <strong>Tech Stack:</strong><br />
+                  - Unity
+                  - Node
+                  - C#
+                  - Android
+                  - IOS
+                  </>}
               // ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
             />
@@ -131,8 +195,16 @@ function Projects() {
 
               videoLink="https://www.youtube.com/embed/jHqQYZ5Pg3U"
               title="Murlan Card Game"
-              description="It is a 2d Builder game in which user manages a farm have in apps sell products and make his farm some top level.
-               leader board and many more apis"
+              description={
+                <>
+                  It is a multiplayer Card Game with functionality of private room making playing with friends and also with chat system.<br />
+                  <strong>Tech Stack:</strong><br />
+                  - Unity
+                  - WebSockets
+                  - C#
+                  - Android
+                </>
+              }
               // ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
               // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
             />
